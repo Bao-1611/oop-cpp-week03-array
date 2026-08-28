107368914fca6c089a07ced25462cbe2a13e2813
Nguyen Chi Bao <ncbslav@gmail.com>
2026-08-28 11:16:13 +0700

    first array codes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 # include <iostream>
 # include <string>
+using namespace std;
 
 int main()
 {
+    string name [20];
+    string ids [20];
 
+    for (int i = 0; i < 20; i++)
+    {
+        cout << "Nhap SV Thu " << i + 1 << endl;
+        cout << "Ten Sinh Vien: ";
+        getline (cin, name[i]);
+        cout << "ID Sinh Vien: ";
+        getline (cin, ids[i]);
+    }
     
+    cout << "--DANH SACH SINH VIEN--" << endl;
+    for (int i = 0; i < 20; i++)
+    {
+        cout << "Sinh Vien " << i + 1 << endl;
+        cout << "Ten: " << name[i] << endl;
+    }
+    return 0;
 }
